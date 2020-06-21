cd9e691f81c58a83ae2e1d4d0de6d6b313972b43
Migi <michnow@o2.pl>
2020-06-21 12:12:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        List<string> words = new List<string>() 
+        {
+            "something", "graduate", "cosmos", "pleasure", "elephant", "balls", "failure", "skateboarding", "cinema", "gargoyle", "kitchen",
+            "crocodile", "pursue", "widow", "quantum", "venom", "robotic", "magician", "building", "quarry", "ethos", "terrific", "minion"
+        };
+        static void Menu()
+        {
+
+        }
+    }
+}
